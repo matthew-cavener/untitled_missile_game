0e56c49efebbe12520f6daf30ee2d14e4c80cbd5
Matthew Cavener <matthew.cavener@gmail.com>
2024-03-12 20:57:14 -0700

    Refactor missile guidance logic and improve intercept calculations

--- enemy/missile/enemy_missile.gd
+++ enemy/missile/enemy_missile.gd
@@ -1,17 +1,19 @@
 extends RigidBody2D
 
-# boost thrust should be on order of deploying ship speed, all times should be ~1-3
+# boost thrust should be on the order of deploying ship speed, all times should be ~1-3
 
 
 @onready var target = get_tree().get_first_node_in_group("player")
+@onready var approx_time_to_collision = (target.global_position - global_position).length() / (linear_velocity.length() + target.linear_velocity.length())
 var boost_thrust_magnitude = 3
 var boost_thrust_time = 3
-var coast_thrust_magnitude = 0
+var midcourse_thrust_magnitude = 0
 var terminal_thrust_magnitude = 2
-var terminal_thrust_time = 2
+var terminal_thrust_time = 5
 var terminal_range = 200
 var is_boosting = false
 var is_terminal = false
+var armed = true
 var boost_timer := Timer.new()
 var terminal_timer := Timer.new()
 
@@ -33,20 +35,17 @@ func proportional_navigation(proportionality_constant = 3):
     var acceleration_commanded = proportionality_constant * relative_velocity.cross(omega)
     return Vector2(acceleration_commanded.x, acceleration_commanded.y)
 
-func get_velocity_rejection():
-    # https://en.wikipedia.org/wiki/Vector_projection#Vector_projection_2
-    var target_direction = global_position.direction_to(target.global_position)
-    var target_direction_unit = target_direction.normalized()
-    var velocity_vector_projection = linear_velocity.dot(target_direction_unit) * target_direction_unit
-    var velocity_vector_rejection = linear_velocity - velocity_vector_projection
-    return velocity_vector_rejection
-
 func get_closing_thrust(stage_thrust, stage_time_remaining):
-    var target_direction = global_position.direction_to(target.global_position)
+    # https://en.wikipedia.org/wiki/Vector_projection#Vector_projection_2
+    # create thrust vector to intercept target by minimizing velocity vector rejection
+    # first order approximation, assumes constant velocity, assumption mostly holds for the short burn times and low acceleration
+    var intercept_position = target.global_position + target.linear_velocity * approx_time_to_collision
+    var intercept_direction = global_position.direction_to(intercept_position)
+    var intercept_direction_unit = intercept_direction.normalized()
     var stage_delta_v = (stage_thrust * stage_time_remaining) / self.mass
-    var velocity_rejection = get_velocity_rejection()
-    # ensure enough of the available thrust is expended to cancel out the velocity rejection, using the rest to close the distance
-    var closing_thrust_direction = (stage_delta_v * target_direction.normalized() - velocity_rejection).normalized()
+    var velocity_vector_projection = linear_velocity.dot(intercept_direction_unit) * intercept_direction_unit
+    var velocity_vector_rejection = linear_velocity - velocity_vector_projection
+    var closing_thrust_direction = (stage_delta_v * intercept_direction_unit - velocity_vector_rejection).normalized()
     var closing_thrust_magnitude = stage_thrust
     var closing_thrust = closing_thrust_direction * closing_thrust_magnitude
     return closing_thrust
@@ -64,28 +63,31 @@ func _ready():
 
 func _integrate_forces(_state):
     target = get_target()
-    var approx_time_to_collision = (target.global_position - global_position).length() / (linear_velocity.length() + target.linear_velocity.length())
+    # first order approximation, assumes constant velocity, assumption mostly holds for the short burn times and low acceleration
+    approx_time_to_collision = (target.global_position - global_position).length() / (linear_velocity.length() + target.linear_velocity.length())
     var applied_forces = Vector2(0,0)
+    is_boosting = false
     if not boost_timer.is_stopped():
         is_boosting = true
         applied_forces += get_closing_thrust(boost_thrust_magnitude, boost_timer.time_left)
-        # apply_central_force(get_closing_thrust(boost_thrust_magnitude, boost_thrust_time))
     elif (target.global_position - global_position).length() < terminal_range:
         is_terminal = true
         if approx_time_to_collision < terminal_thrust_time:
             is_boosting = true
             applied_forces += get_closing_thrust(terminal_thrust_magnitude, approx_time_to_collision)
-            # apply_central_force(get_closing_thrust(terminal_thrust_magnitude, approx_time_to_collision))
-    else:
-        is_boosting = false
+    elif midcourse_thrust_magnitude > 0:
         var prop_nav_acceleration = proportional_navigation()
         var prop_nav_thrust = prop_nav_acceleration * self.mass
-        if prop_nav_thrust.length() > coast_thrust_magnitude:
-            prop_nav_thrust = prop_nav_thrust.normalized() * coast_thrust_magnitude
-        if prop_nav_thrust.length() < 0.8 * coast_thrust_magnitude:
+        if prop_nav_thrust.length() < 0.8 * midcourse_thrust_magnitude:
             prop_nav_thrust = Vector2(0,0)
+        elif prop_nav_thrust.length() > midcourse_thrust_magnitude:
+            prop_nav_thrust = prop_nav_thrust.normalized() * midcourse_thrust_magnitude
+        if prop_nav_thrust.length() > 0:
+            is_boosting = true
         applied_forces += prop_nav_thrust
-        # apply_central_force(prop_nav_thrust)
+    elif is_terminal:
+        is_terminal = false
+        armed = false
     apply_central_force(applied_forces)
     print("\n----------------")
     print("target:" + str(target))
