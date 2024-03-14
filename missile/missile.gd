19140d8ae4de05ebfa7adbd52bad5bb02aebc787
Matthew Cavener <matthew.cavener@gmail.com>
2024-03-13 20:14:06 -0700

    add type annotations to missile

--- missile/missile.gd
+++ missile/missile.gd
@@ -12,39 +12,39 @@ extends RigidBody2D
 
 enum MissileState {BOOSTING, MIDCOURSE, SEEKING, TERMINAL, DISARMED}
 
-var state = MissileState.BOOSTING
+var state: MissileState = MissileState.BOOSTING
 
-var intended_target
-var valid_targets
+var intended_target: String
+var valid_targets: Array
 
-@onready var target = get_tree().get_first_node_in_group(intended_target)
-@onready var approx_time_to_collision = (target.global_position - global_position).length() / (linear_velocity.length() + target.linear_velocity.length())
+@onready var target: Node = get_tree().get_first_node_in_group(intended_target)
+@onready var approx_time_to_collision: float = (target.global_position - global_position).length() / (linear_velocity.length() + target.linear_velocity.length())
 
-var total_thrust_time = 0 # player missiles only
+var total_thrust_time: int = 0 # player missiles only
 
-var boost_thrust_magnitude
-var boost_thrust_time
-var midcourse_thrust_magnitude
-var terminal_thrust_magnitude
-var terminal_thrust_time
-var terminal_range = 200
+var boost_thrust_magnitude: float
+var boost_thrust_time: float
+var midcourse_thrust_magnitude: float
+var terminal_thrust_magnitude: float
+var terminal_thrust_time: float
+var terminal_range: int = 200
 
-var is_boosting = false
-var is_terminal = false
-var armed = true
+var is_boosting: bool = false
+var is_terminal: bool = false
+var armed: bool = true
 
-var boost_thrust_timer := Timer.new()
-var terminal_thrust_timer := Timer.new()
+var boost_thrust_timer: Timer = Timer.new()
+var terminal_thrust_timer: Timer = Timer.new()
 
 func _init(
-    _intended_target = "player",
-    _valid_targets = ["decoys", "player"],
-    _boost_thrust_magnitude = 3,
-    _boost_thrust_time = 3,
-    _midcourse_thrust_magnitude = 0,
-    _terminal_thrust_magnitude = 3,
-    _terminal_thrust_time = 3
-):
+    _intended_target: String = "player",
+    _valid_targets: Array = ["decoys", "player"],
+    _boost_thrust_magnitude: float = 3,
+    _boost_thrust_time: float = 3,
+    _midcourse_thrust_magnitude: float = 0,
+    _terminal_thrust_magnitude: float = 3,
+    _terminal_thrust_time: float = 3
+) -> void:
     intended_target = _intended_target
     valid_targets = _valid_targets
     boost_thrust_magnitude = _boost_thrust_magnitude
@@ -53,7 +53,7 @@ func _init(
     terminal_thrust_magnitude = _terminal_thrust_magnitude
     terminal_thrust_time = _terminal_thrust_time
 
-func get_target():
+func get_target() -> Node:
     for valid_target in valid_targets:
         if valid_target != intended_target:
             target = get_tree().get_first_node_in_group(valid_target)
@@ -61,40 +61,29 @@ func get_target():
                 return target
     return get_tree().get_first_node_in_group(intended_target)
 
-func proportional_navigation(proportionality_constant = 3):
+func proportional_navigation(proportionality_constant: float = 3) -> Vector2:
     # https://en.wikipedia.org/wiki/Proportional_navigation
-    var relative_velocity = target.linear_velocity - linear_velocity
-    var target_range = target.global_position - global_position
-    relative_velocity = Vector3(relative_velocity.x, relative_velocity.y, 0)
-    target_range = Vector3(target_range.x, target_range.y, 0)
-    var omega = target_range.cross(relative_velocity) / target_range.dot(target_range)
-    var acceleration_commanded = proportionality_constant * relative_velocity.cross(omega)
+    var relative_velocity2: Vector2 = target.linear_velocity - linear_velocity
+    var target_range2: Vector2 = target.global_position - global_position
+    var relative_velocity3 = Vector3(relative_velocity2.x, relative_velocity2.y, 0)
+    var target_range3 = Vector3(target_range2.x, target_range2.y, 0)
+    var omega: Vector3 = target_range3.cross(relative_velocity3) / target_range3.dot(target_range3)
+    var acceleration_commanded: Vector3 = proportionality_constant * relative_velocity3.cross(omega)
     return Vector2(acceleration_commanded.x, acceleration_commanded.y)
 
-func get_closing_thrust(stage_thrust, stage_time_remaining):
-    # https://en.wikipedia.org/wiki/Vector_projection#Vector_projection_2
-    # create thrust vector to intercept target by minimizing velocity vector rejection
-    # first order approximation, assumes constant velocity, assumption mostly holds for the short burn times and low acceleration
-    var intercept_position = target.global_position + target.linear_velocity * approx_time_to_collision
-    var intercept_direction = global_position.direction_to(intercept_position)
-    var intercept_direction_unit = intercept_direction.normalized()
-    var stage_delta_v = (stage_thrust * stage_time_remaining) / self.mass
-    var velocity_vector_projection = linear_velocity.dot(intercept_direction_unit) * intercept_direction_unit
-    var velocity_vector_rejection = linear_velocity - velocity_vector_projection
-    var closing_thrust_direction = (stage_delta_v * intercept_direction_unit - velocity_vector_rejection).normalized()
-    var closing_thrust_magnitude = stage_thrust
-    var closing_thrust = closing_thrust_direction * closing_thrust_magnitude
+func get_closing_thrust(stage_thrust: float, stage_time_remaining: float) -> Vector2:
+    var intercept_position: Vector2 = target.global_position + target.linear_velocity * approx_time_to_collision
+    var intercept_direction: Vector2 = global_position.direction_to(intercept_position)
+    var intercept_direction_unit: Vector2 = intercept_direction.normalized()
+    var stage_delta_v: float = (stage_thrust * stage_time_remaining) / self.mass
+    var velocity_vector_projection: Vector2 = linear_velocity.dot(intercept_direction_unit) * intercept_direction_unit
+    var velocity_vector_rejection: Vector2 = linear_velocity - velocity_vector_projection
+    var closing_thrust_direction: Vector2 = (stage_delta_v * intercept_direction_unit - velocity_vector_rejection).normalized()
+    var closing_thrust_magnitude: float = stage_thrust
+    var closing_thrust: Vector2 = closing_thrust_direction * closing_thrust_magnitude
     return closing_thrust
 
-func _ready():
-    print("\n----------------")
-    print("missile spwaned")
-    print("global_position.x: " + str(global_position.x) + " | global_position.y:" + str(global_position.y))
-    print("linear_velocity.x: " + str(linear_velocity.x) + " | linear_velocity.y:" + str(linear_velocity.y))
-    print("----------------\n")
-    # if total_thrust_time > 0:
-    #     boost_thrust_time = boost_thrust_time
-    #     terminal_thrust_time = total_thrust_time - boost_thrust_time
+func _ready() -> void:
     boost_thrust_timer.wait_time = boost_thrust_time
     boost_thrust_timer.one_shot = true
     add_child(boost_thrust_timer)
@@ -106,16 +95,16 @@ func _ready():
     add_child(terminal_thrust_timer)
     terminal_thrust_timer.timeout.connect(_on_terminal_thrust_timer_timeout)
 
-func _on_boost_thrust_timer_timeout():
+func _on_boost_thrust_timer_timeout() -> void:
     state = MissileState.MIDCOURSE
 
-func _on_terminal_thrust_timer_timeout():
+func _on_terminal_thrust_timer_timeout() -> void:
     state = MissileState.DISARMED
 
-func _integrate_forces(_state):
+func _integrate_forces(_state) -> void:
     target = get_target()
     approx_time_to_collision = (target.global_position - global_position).length() / (linear_velocity.length() + target.linear_velocity.length())
-    var applied_forces = Vector2(0,0)
+    var applied_forces: Vector2 = Vector2(0,0)
 
     match state:
         MissileState.BOOSTING:
