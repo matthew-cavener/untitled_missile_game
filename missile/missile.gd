d946fec46074d831728870df1076cf9e70748d13
Matthew Cavener <matthew.cavener@gmail.com>
2024-03-13 20:50:15 -0700

    Refactor missile state handling and add state name mapping

--- missile/missile.gd
+++ missile/missile.gd
@@ -1,11 +1,11 @@
-# boost_thrust_magnitude should be on order of relative velocity between launching platform and target
+# boost_thrust_magnitude * boost_thrust_time should be on order of relative velocity between launching platform and target to ensure enough delta_v is available to cancel orthogonal velocity
 # boost_thrust_time should be short to minimize visibility to target
 # for single stage missile boost_thrust_magnitude = terminal_thrust_magnitude
     # make sure to assign enough boost_thrust_time to ensure intercept
     # player will select boost and terminal thrust times from a total thrust time budget
 # missile difficulty is proportional to boost_thrust_magnitude * boost_thrust_time + terminal_thrust_magnitude * terminal_thrust_time
 
-# midcourse_thrust_magnitude should be ~0.1 and reserved for anti-missile missiles
+# midcourse_thrust_magnitude should be <1 and reserved for anti-missile missiles
 
 class_name Missile
 extends RigidBody2D
@@ -13,6 +13,17 @@ extends RigidBody2D
 enum MissileState {BOOSTING, MIDCOURSE, SEEKING, TERMINAL, DISARMED}
 
 var state: MissileState = MissileState.BOOSTING
+
+var state_names = {
+    MissileState.BOOSTING: "BOOSTING",
+    MissileState.MIDCOURSE: "MIDCOURSE",
+    MissileState.SEEKING: "SEEKING",
+    MissileState.TERMINAL: "TERMINAL",
+    MissileState.DISARMED: "DISARMED"
+}
+
+func get_current_state_name() -> String:
+    return state_names[state]
 
 var intended_target: String
 var valid_targets: Array
@@ -28,10 +39,6 @@ var midcourse_thrust_magnitude: float
 var terminal_thrust_magnitude: float
 var terminal_thrust_time: float
 var terminal_range: int = 200
-
-var is_boosting: bool = false
-var is_terminal: bool = false
-var armed: bool = true
 
 var boost_thrust_timer: Timer = Timer.new()
 var terminal_thrust_timer: Timer = Timer.new()
@@ -61,7 +68,7 @@ func get_target() -> Node:
                 return target
     return get_tree().get_first_node_in_group(intended_target)
 
-func proportional_navigation(proportionality_constant: float = 3) -> Vector2:
+func proportional_navigation(proportionality_constant: int = 3) -> Vector2:
     # https://en.wikipedia.org/wiki/Proportional_navigation
     var relative_velocity2: Vector2 = target.linear_velocity - linear_velocity
     var target_range2: Vector2 = target.global_position - global_position
@@ -72,6 +79,9 @@ func proportional_navigation(proportionality_constant: float = 3) -> Vector2:
     return Vector2(acceleration_commanded.x, acceleration_commanded.y)
 
 func get_closing_thrust(stage_thrust: float, stage_time_remaining: float) -> Vector2:
+    # https://en.wikipedia.org/wiki/Vector_projection#Vector_projection_2
+    # create thrust vector to intercept target by minimizing orthogonal velocity
+    # first order approximation, assumes constant velocity, assumption mostly holds for the short burn times and low acceleration
     var intercept_position: Vector2 = target.global_position + target.linear_velocity * approx_time_to_collision
     var intercept_direction: Vector2 = global_position.direction_to(intercept_position)
     var intercept_direction_unit: Vector2 = intercept_direction.normalized()
@@ -82,6 +92,12 @@ func get_closing_thrust(stage_thrust: float, stage_time_remaining: float) -> Vec
     var closing_thrust_magnitude: float = stage_thrust
     var closing_thrust: Vector2 = closing_thrust_direction * closing_thrust_magnitude
     return closing_thrust
+
+func _on_boost_thrust_timer_timeout() -> void:
+    state = MissileState.MIDCOURSE
+
+func _on_terminal_thrust_timer_timeout() -> void:
+    state = MissileState.DISARMED
 
 func _ready() -> void:
     boost_thrust_timer.wait_time = boost_thrust_time
@@ -95,12 +111,6 @@ func _ready() -> void:
     add_child(terminal_thrust_timer)
     terminal_thrust_timer.timeout.connect(_on_terminal_thrust_timer_timeout)
 
-func _on_boost_thrust_timer_timeout() -> void:
-    state = MissileState.MIDCOURSE
-
-func _on_terminal_thrust_timer_timeout() -> void:
-    state = MissileState.DISARMED
-
 func _integrate_forces(_state) -> void:
     target = get_target()
     approx_time_to_collision = (target.global_position - global_position).length() / (linear_velocity.length() + target.linear_velocity.length())
@@ -108,13 +118,9 @@ func _integrate_forces(_state) -> void:
 
     match state:
         MissileState.BOOSTING:
-            is_boosting = true
-            is_terminal = false
             applied_forces += get_closing_thrust(boost_thrust_magnitude, boost_thrust_timer.time_left)
 
         MissileState.MIDCOURSE:
-            is_boosting = false
-            is_terminal = false
             if (target.global_position - global_position).length() < terminal_range:
                 state = MissileState.SEEKING
             elif midcourse_thrust_magnitude > 0:
@@ -125,29 +131,22 @@ func _integrate_forces(_state) -> void:
                     applied_forces += prop_nav_thrust
 
         MissileState.SEEKING:
-            is_boosting = false
-            is_terminal = true
             if approx_time_to_collision < terminal_thrust_time:
                 terminal_thrust_timer.start()
                 state = MissileState.TERMINAL
 
         MissileState.TERMINAL:
-            is_boosting = true
-            is_terminal = true
             applied_forces += get_closing_thrust(terminal_thrust_magnitude, approx_time_to_collision)
 
         MissileState.DISARMED:
-            is_boosting = false
-            is_terminal = false
-            armed = false
+            pass
 
     apply_central_force(applied_forces)
     print("\n----------------")
     print("target:" + str(target))
     print("global_position.x: " + str(global_position.x) + " | global_position.y:" + str(global_position.y))
     print("linear_velocity.x: " + str(linear_velocity.x) + " | linear_velocity.y:" + str(linear_velocity.y))
-    print("is_boosting:" + str(is_boosting))
-    print("is_terminal:" + str(is_terminal))
+    print("state: " + str(get_current_state_name()))
     print("approx_time_to_collision: " + str(approx_time_to_collision) + " seconds")
     print("missile force applied: " + str(applied_forces))
     print("missile force magnitude: " + str(applied_forces.length()))
