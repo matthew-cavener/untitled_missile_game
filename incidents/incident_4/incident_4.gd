b10fc7afe6d51bd14e0a45511db759cb900922b8
Matthew Cavener <matthew.cavener@gmail.com>
2024-12-10 10:29:23 -0800

    switch incident 4 and 5, add ciws to incidents 4, 5, 6

--- incidents/incident_4/incident_4.gd
+++ incidents/incident_4/incident_4.gd
@@ -5,14 +5,47 @@ var player_ship_scene = preload("res://player/ship/player_ship.tscn")
 @onready var world: Node2D = get_tree().get_first_node_in_group("world")
 
 var resources_expended = 0
+var ciws_available = true
 var salary = 2320
 var max_bonus = 13333 - salary
 var incident_started = false
 var incident_resolved = false
-var incident_resolution_time = 25
+var incident_resolution_time = 50
 var incident_timer = Timer.new()
 
 var player_ship_tube_1_contents = {
+    "type": "decoy",
+    "display_name": "Mk 36 SRBOC\nChaff\nCost: 300",
+    "resource_cost": 300,
+    "group": "chaff",
+    "launch_bearing": 270,
+}
+
+var player_ship_tube_2_contents = {
+    "type": "decoy",
+    "display_name": "Mk 214 BSIRCM\nFlare\nCost: 300",
+    "resource_cost": 300,
+    "group": "flare",
+    "launch_bearing": 90,
+}
+
+var player_ship_tube_4_contents = {
+    "type": "decoy",
+    "display_name": "Mk 36 SRBOC\nChaff\nCost: 300",
+    "resource_cost": 300,
+    "group": "chaff",
+    "launch_bearing": 270,
+}
+
+var player_ship_tube_5_contents = {
+    "type": "decoy",
+    "display_name": "Mk 214 BSIRCM\nFlare\nCost: 300",
+    "resource_cost": 300,
+    "group": "flare",
+    "launch_bearing": 90,
+}
+
+var player_ship_tube_6_contents = {
     "type": "missile",
     "display_name": "VACM-161 SM-3 Block IIA\nAutonomous Missile Interceptor\nCost: 10k",
     "resource_cost": 10000,
@@ -27,30 +60,28 @@ var player_ship_tube_1_contents = {
     "velocity_rejection_coefficient": 3.0
 }
 
-var player_ship_tube_4_contents = {
-    "type": "decoy",
-    "display_name": "Mk 36 SRBOC\nChaff\nCost: 300",
-    "resource_cost": 300,
-    "group": "chaff",
-    "launch_bearing": 180,
-}
-
-var player_ship_tube_5_contents = {
-    "type": "decoy",
-    "display_name": "Mk 214 BSIRCM\nFlare\nCost: 300",
-    "resource_cost": 300,
-    "group": "flare",
-    "launch_bearing": 150,
-}
-
 var enemy_ship_1_missiles = [
     {
-        "seeker_has_ping": false,
+        "seeker_has_ping": true,
         "group": "enemy_missiles",
         "intended_target": "player",
-        "countermeasures": ["flare"],
-        "stowed_time": 9,
-        "boost_thrust_magnitude": 9.0,
+        "countermeasures": ["chaff"],
+        "stowed_time": 12,
+        "boost_thrust_magnitude": 2.0,
+        "boost_thrust_time": 6.0,
+        "maneuvering_thrust_magnitude": 0.3,
+        "terminal_thrust_time": 3.0,
+        "terminal_thrust_magnitude": 1.0,
+        "seeker_range": 120,
+        "velocity_rejection_coefficient": 1.2
+    },
+    {
+        "seeker_has_ping": true,
+        "group": "enemy_missiles",
+        "intended_target": "player",
+        "countermeasures": ["chaff"],
+        "stowed_time": 40,
+        "boost_thrust_magnitude": 12.0,
         "boost_thrust_time": 3.0,
         "maneuvering_thrust_magnitude": 0.3,
         "terminal_thrust_time": 3.0,
@@ -60,12 +91,37 @@ var enemy_ship_1_missiles = [
     },
 ]
 
+var enemy_ship_2_missiles = [
+    {
+        "seeker_has_ping": false,
+        "group": "enemy_missiles",
+        "intended_target": "player",
+        "countermeasures": ["flare"],
+        "stowed_time": 12.2,
+        "boost_thrust_magnitude": 2.0,
+        "boost_thrust_time": 5.0,
+        "maneuvering_thrust_magnitude": 0.3,
+        "terminal_thrust_time": 3.0,
+        "terminal_thrust_magnitude": 1.0,
+        "seeker_range": 120,
+        "velocity_rejection_coefficient": 1.2
+    }
+]
+
 var enemy_ship_1_parameters = {
-    "distance": 600,
-    "position_bearing": 60,
-    "speed": 30,
-    "velocity_bearing": 45 + 180,
+    "distance": 380,
+    "position_bearing": 15,
+    "speed": 12,
+    "velocity_bearing": 180,
     "missiles": enemy_ship_1_missiles
+}
+
+var enemy_ship_2_parameters = {
+    "distance": 380,
+    "position_bearing": 360 - 15,
+    "speed": 12,
+    "velocity_bearing": 180,
+    "missiles": enemy_ship_2_missiles
 }
 
 func get_details():
@@ -73,9 +129,10 @@ func get_details():
         "name": "Incident 4",
         "description": "
             Date: 1993-08-03
-            Location: Low Ganymede Orbit\nLGO-384 rendezvous transfer
-            Incident Report: Single RADAR contact
-            One passive IR homing missile deployed by contact,
+            Location: Low Jupiter Orbit LJO-0 station
+            Incident Report: Two RADAR contacts
+            Three Active RADAR homing missiles deployed by contacts,
+            One Passive IR homing missiles deployed by contacts,
             No casualties.
             Resource expended: " + str(resources_expended),
         "resources_expended": resources_expended,
@@ -100,14 +157,26 @@ func _on_incident_4_begin():
     incident_timer.start()
     var player_ship = player_ship_scene.instantiate()
     var enemy_ship_1 = enemy_ship_scene.instantiate()
+    var enemy_ship_2 = enemy_ship_scene.instantiate()
     add_child(player_ship)
     add_child(enemy_ship_1)
-    player_ship.set_parameters({"tube_1_contents": player_ship_tube_1_contents, "tube_4_contents": player_ship_tube_4_contents, "tube_5_contents": player_ship_tube_5_contents})
+    add_child(enemy_ship_2)
+    player_ship.set_parameters({
+        "tube_1_contents": player_ship_tube_1_contents,
+        "tube_2_contents": player_ship_tube_2_contents,
+        "tube_4_contents": player_ship_tube_4_contents,
+        "tube_5_contents": player_ship_tube_5_contents,
+        "tube_6_contents": player_ship_tube_6_contents,
+        "ciws_available": ciws_available
+    })
     enemy_ship_1.set_parameters(enemy_ship_1_parameters)
+    enemy_ship_2.set_parameters(enemy_ship_2_parameters)
+    Events.emit_signal("ciws_available", ciws_available)
 
 func _on_incident_4_resolved():
     incident_resolved = true
     Events.emit_signal("incident_resolved")
+    Events.emit_signal("ciws_available", false)
     incident_timer.queue_free()
     for child in get_children():
         if child.has_method("set_parameters"):
