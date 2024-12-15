cab0b5a66727c48770b6db43beba62588050b6d4
Matthew Cavener <matthew.cavener@gmail.com>
2024-12-14 18:24:52 -0800

    flash timer changes

--- ui/numerical_display.gd
+++ ui/numerical_display.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @export var flashes: int = 3
-@export var flash_delay: float = 0.333
+@export var total_flash_time: float = 1.0
 @export var reveal_time: float = 0.333
 @export var new_value: String = "123456"
 
@@ -9,26 +9,24 @@ extends Control
 var is_updating: bool = false
 var current_timers = []
 
-# func _ready() -> void:
-#     update_display(flashes, flash_delay, new_value, reveal_time)
-
-func _flash(flashes: int, delay: float, new_value: String, flash_count: int, reveal_time: float) -> void:
+func _flash(flashes: int, total_flash_time: float, new_value: String, flash_count: int, reveal_time: float) -> void:
+    var flash_delay = total_flash_time / (flashes * 2)
     if flash_count < flashes * 2:
         lit_segments.visible = not lit_segments.visible
         flash_count += 1
         var timer = Timer.new()
         add_child(timer)
         current_timers.append(timer)
-        timer.wait_time = delay
+        timer.wait_time = flash_delay
         timer.one_shot = true
-        timer.connect("timeout", _on_flash_timeout.bind(flashes, delay, new_value, flash_count, reveal_time))
+        timer.connect("timeout", _on_flash_timeout.bind(flashes, total_flash_time, new_value, flash_count, reveal_time))
         timer.start()
     else:
         lit_segments.visible = true
         _reveal(new_value, reveal_time)
 
-func _on_flash_timeout(flashes: int, delay: float, new_value: String, flash_count: int, reveal_time: float) -> void:
-    _flash(flashes, delay, new_value, flash_count, reveal_time)
+func _on_flash_timeout(flashes: int, total_flash_time: float, new_value: String, flash_count: int, reveal_time: float) -> void:
+    _flash(flashes, total_flash_time, new_value, flash_count, reveal_time)
 
 func _reveal(new_value: String, reveal_time: float) -> void:
     lit_segments.text = new_value
@@ -56,17 +54,14 @@ func _reveal_step(new_value: String, reveal_index: int, reveal_delay: float) -> 
 func _on_reveal_step_timeout(new_value: String, reveal_index: int, reveal_delay: float) -> void:
     _reveal_step(new_value, reveal_index, reveal_delay)
 
-func update_display(flashes: int, delay: float, new_value: String, reveal_time: float) -> void:
+func update_display(flashes: int, total_flash_time: float, new_value: String, reveal_time: float) -> void:
     if is_updating:
         _cleanup_timers()
     is_updating = true
     var flash_count = 0
-    _flash(flashes, delay, new_value, flash_count, reveal_time)
+    _flash(flashes, total_flash_time, new_value, flash_count, reveal_time)
 
 func _cleanup_timers() -> void:
     for timer in current_timers:
         timer.queue_free()
     current_timers.clear()
-
-func _on_button_pressed() -> void:
-    update_display(6, 0.167, "hello", 0.167)
